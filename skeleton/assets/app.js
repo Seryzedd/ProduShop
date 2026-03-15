42767d32d552ce7bbe38b07ee1746f5e50a7dee4
Seryzedd <aurelienbard@hotmail.fr>
2026-03-15 14:49:49 +0100

    Add scroll to top button

--- skeleton/assets/app.js
+++ skeleton/assets/app.js
@@ -223,14 +223,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    $(mybutton).fadeIn();
   } else {
-    mybutton.style.display = "none";
+    $(mybutton).fadeOut();
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+mybutton.addEventListener('click', function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
