744956a767ec698e13c929a80b55353ff3249eb2
Seryzedd <aurelienbard@hotmail.fr>
2026-03-15 14:30:43 +0100

    Add alert on not completed account

--- skeleton/assets/app.js
+++ skeleton/assets/app.js
@@ -7,6 +7,7 @@
 import $ from 'jquery';
 
 import 'bootstrap';
+import * as bootstrap from 'bootstrap';
 
 import './styles/app.css';
 // import './styles/app.scss';
@@ -184,8 +185,52 @@ async function sendRequest(url, params) {
     }
 
     const result = await response.json();
-    console.log(result);
   } catch (error) {
     console.error(error.message);
   }
 }
+
+// popover
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('[data-bs-toggle="popover"]').forEach(el => {
+    const pop = new bootstrap.Popover(el, { trigger: 'manual' });
+
+    el.addEventListener('shown.bs.popover', () => {
+      const tipId = el.getAttribute('aria-describedby');
+      const tip = document.getElementById(tipId);
+      if (!tip) return;
+
+      // Récupère la classe popover-* sur le lien
+      const popoverClass = [...el.classList].find(cls => cls.startsWith('popover-'));
+      if (!popoverClass) return;
+
+      // On ajoute simplement la classe sur le tip
+      tip.classList.add(popoverClass);
+    });
+
+    pop.show();
+    el.addEventListener('click', e => { e.preventDefault(); pop.toggle(); });
+  });
+});
+
+// Scroll to top
+
+let mybutton = document.getElementById("top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
